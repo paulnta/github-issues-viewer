6403f7c7900f9201c80f32f16c0a8038aeffdee0
paulnta <paul.ntawuruhunga@heig-vd.ch>
2018-11-22 04:11:24 +0100

    refactor skeleton utils

--- src/components/Skeleton.js
+++ src/components/Skeleton.js
@@ -1,16 +1,15 @@
 import { createSkeletonProvider, createSkeletonElement } from "@trainline/react-skeletor";
 import AvatarBase from './Avatar';
 
-const defaultPredicate = props => props.loading;
-const defaultStyling = 'skeleton-pending';
-
 export const Span = createSkeletonElement('span');
 export const Avatar = createSkeletonElement(AvatarBase);
 
 export const placeholder = length => 'x '.repeat(length / 2);
 
-export const withSkeletonProvider = (dummyData, predicate = defaultPredicate, styling = defaultStyling) => {
-  return createSkeletonProvider(dummyData, predicate, styling);
-};
-
 export const withSkeleton = createSkeletonElement;
+
+export const withSkeletonProvider = (
+  dummyData = {},
+  predicate = props => props.loading,
+  styling = 'skeleton-pending',
+) => createSkeletonProvider(dummyData, predicate, styling);
