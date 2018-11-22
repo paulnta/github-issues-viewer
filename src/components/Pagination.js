437b4bdd970c2e742c5d3a853a5b29bc38c34d59
paulnta <paul.ntawuruhunga@heig-vd.ch>
2018-11-22 04:10:33 +0100

    added issues pagination

---
+++ src/components/Pagination.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Toolbar from '@material-ui/core/Toolbar';
+
+const styles = theme => ({
+  root: {
+    justifyContent: 'center',
+  },
+});
+
+const Pagination = ({ classes, children, onLoadMore, hasNextPage }) => (
+  <Toolbar className={classes.root}>
+    <Button
+      disabled={!hasNextPage}
+      onClick={onLoadMore}
+      color="primary"
+    >
+      Load more
+    </Button>
+  </Toolbar>
+);
+
+Pagination.propTypes = {
+  classes: PropTypes.objectOf(PropTypes.string),
+  children: PropTypes.node,
+  disabled: PropTypes.bool,
+  onLoadMore: PropTypes.func,
+};
+
+export default withStyles(styles)(Pagination);
