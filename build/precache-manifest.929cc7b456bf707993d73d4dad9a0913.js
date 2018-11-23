1b47a1dac1fb844dd463ab4c8f9092526a0c335d
paulnta <paul.ntawuruhunga@heig-vd.ch>
2018-11-23 09:30:46 +0100

    added build

---
+++ build/precache-manifest.929cc7b456bf707993d73d4dad9a0913.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6f19a3ab17afd14157fe",
+    "url": "/static/js/main.6f19a3ab.chunk.js"
+  },
+  {
+    "revision": "4c38b02ce484bfbc5c74",
+    "url": "/static/js/1.4c38b02c.chunk.js"
+  },
+  {
+    "revision": "39606c8a94187b366855305c3a28a7ec",
+    "url": "/index.html"
+  }
+];
