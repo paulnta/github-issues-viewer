d9847432676f42baa2ebf63a451ab5ceb0ed8c76
paulnta <paul.ntawuruhunga@heig-vd.ch>
2018-11-26 17:49:35 +0100

    added analytics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
 import React, { Component } from "react";
-import Layout from "./components/Layout";
-import Routes from "./routes";
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import ReactGA from 'react-ga';
+import Layout from './components/Layout';
+import Routes from './routes';
+
+const trackingCode = process.env.REACT_APP_GOOGLE_TRACKING_ID;
+let trackPage = () => { };
+
+if (trackingCode) {
+  // Initialize analytics
+  ReactGA.initialize(trackingCode);
+  trackPage = page => {
+    if (trackingCode) {
+      ReactGA.set({ page });
+      ReactGA.pageview(page);
+    }
+  };
+}
 
 class App extends Component {
+  static propTypes = {
+    location: PropTypes.shape({
+      pathname: PropTypes.string.isRequired,
+      search: PropTypes.string.isRequired,
+    }).isRequired,
+  }
+
+  componentDidMount() {
+    const { location } = this.props;
+    const page = `${location.pathname}${location.search}`;
+    trackPage(page);
+  }
+
+  componentDidUpdate(prevProps) {
+    const { location: prevLocation } = prevProps;
+    const { location: nextLocation } = this.props;
+    const currentPage = `${prevLocation.pathname}${prevLocation.search}`;
+    const nextPage = `${nextLocation.pathname}${nextLocation.search}`;
+
+    if (currentPage !== nextPage) {
+      trackPage(nextPage);
+    }
+  }
+
   render() {
     return (
       <Layout>
@@ -12,4 +53,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withRouter(App);
